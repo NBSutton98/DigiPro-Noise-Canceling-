30cd2ccb9a9b1cfc26ddf924bcaacde094d14f9d
Nicholas Sutton <nsutton7@my.bcit.ca>
2025-11-04 13:10:18 -0800

    Libraries and Boilerplate files created

---
+++ AudioEngine.hpp
@@ -0,0 +1,16 @@
+//
+// Created by SBNut on 2025-11-04.
+//
+
+#ifndef AUDIOENGINE_HPP
+#define AUDIOENGINE_HPP
+
+
+
+class AudioEngine {
+
+};
+
+
+
+#endif //AUDIOENGINE_HPP
---
+++ NLMSFilter.hpp
@@ -0,0 +1,16 @@
+//
+// Created by SBNut on 2025-11-04.
+//
+
+#ifndef NLMSFILTER_HPP
+#define NLMSFILTER_HPP
+
+
+
+class NLMSFilter {
+
+};
+
+
+
+#endif //NLMSFILTER_HPP
